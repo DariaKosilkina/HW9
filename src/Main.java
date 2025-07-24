eecd70689d7c73c910f1169749a1bba4fc3dfc8d
DariaKosilkina <d.kosilkina@mail.ru>
2025-07-24 14:40:45 +0300

    /

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+    //task1
+        int[] payments = {10000, 20000, 15000, 30000, 25000};
+        int totalSum = 0;
+        for (int expense : payments) {
+            totalSum += expense;
+        }
+        System.out.println("Сумма трат за месяц составила " + totalSum + " рублей");
+    //task2
+
+    }
+}
